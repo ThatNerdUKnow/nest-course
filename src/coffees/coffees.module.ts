316c37cb1a4c383409a4e81b0813d6c35c4fe04b
Brandon Piña <brandonpi265@gmail.com>
2022-04-30 10:42:20 -0500

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,13 +5,12 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity'
-
-class MockCoffesService { }
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [{ provide: CoffeesService, useValue: new MockCoffesService() }],
+    providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
     exports: [CoffeesService]
 })
 export class CoffeesModule { }
