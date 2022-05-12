912190d3bfb3e22108d637101fad068db711ea4a
Brandon Piña <brandonpi265@gmail.com>
2022-05-11 22:52:30 -0500

    Protect Routes with Guards

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { APP_FILTER, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,9 @@ async function bootstrap() {
     }
   }));
 
-  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalGuards(new ApiKeyGuard())
   await app.listen(3000);
 }
 bootstrap();
