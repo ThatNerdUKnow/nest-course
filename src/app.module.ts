18d8bcfecce1dbc56c06a771ca2c1b354572485e
Brandon Piña <brandonpi265@gmail.com>
2022-05-11 21:21:46 -0500

    Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,27 +10,27 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    }),
     ConfigModule.forRoot({
-      load:[appConfig],
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.required(),
         DATABASE_PORT: Joi.number().default(5432)
       })
-      // Custom env file path
-      //envFilePath:
-      // Ignore env file if using container
-      //ignoreEnvFile: true
     }),
-    CoffeesModule, TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true
-    }), CoffeeRatingModule],
+    CoffeesModule,
+    CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
